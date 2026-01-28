0268c3aeae6921cb41c2e6d42defe2fa089b477a
Jakub Wierzchowiec <jwierzchowiec@griddynamics.com>
2026-01-28 12:09:18 +0100

    task 1

--- task1/task1.test.ts
+++ task1/task1.test.ts
@@ -1,14 +1,68 @@
 import { validateUserName } from 'tasks/task1/index';
 import { fetchIsUserNameAvailable } from 'tasks/task1/fetchIsUserNameValid';
 
-jest.mock('task2/fetchIsUserNameValid', 
-  // write your mock here
-);
+jest.mock('task1/fetchIsUserNameValid');
 
-describe('task2', () => {
-  it('returns false if name has length less than 3 symbols', () => {
-    // write your first unit test here
+describe('task1', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
   });
 
-  // ...and add more test cases
+  it.each(['', 'a', 'ab'])('should return false if user name has length less than 3 symbols [%s]', (userName) => {
+    const mock = jest.mocked(fetchIsUserNameAvailable);
+    const actual = validateUserName(userName);
+
+    expect(actual).resolves.toBe(false);
+    expect(mock).not.toHaveBeenCalled();
+  });
+
+  it('should return false if user name contains spaces', () => {
+    const mock = jest.mocked(fetchIsUserNameAvailable);
+    const userName = 'jakub wierzchowiec 123';
+
+    const actual = validateUserName(userName);
+
+    expect(actual).resolves.toBe(false);
+    expect(mock).not.toHaveBeenCalled();
+  });
+
+  it.each(Array.from({ length: 10 }, (_, i) => i))('should return false if user name starts with a number [%s]', (n) => {
+    const mock = jest.mocked(fetchIsUserNameAvailable);
+    const userName = `${n}jakub123`;
+
+    const actual = validateUserName(userName);
+
+    expect(actual).resolves.toBe(false);
+    expect(mock).not.toHaveBeenCalled();
+  });
+
+  it('should return false if user name is valid but unavailable', () => {
+    const mock = jest.mocked(fetchIsUserNameAvailable).mockResolvedValue(false);
+    const userName = 'jakub123';
+
+    const actual = validateUserName('jakub123');
+
+    expect(actual).resolves.toBe(false);
+    expect(mock).toHaveBeenCalledTimes(1);
+  });
+
+  it('should return true if user name is valid and available', () => {
+    const mock = jest.mocked(fetchIsUserNameAvailable).mockResolvedValue(true);
+    const userName = 'jakub123';
+
+    const actual = validateUserName('jakub123');
+
+    expect(actual).resolves.toBe(true);
+    expect(mock).toHaveBeenCalledTimes(1);
+  });
+
+  it('should return false if user name avaiability check fails', () => {
+    const mock = jest.mocked(fetchIsUserNameAvailable).mockRejectedValue(null);
+    const userName = 'jakub123';
+
+    const actual = validateUserName('jakub123');
+
+    expect(actual).resolves.toBe(false);
+    expect(mock).toHaveBeenCalledTimes(1);
+  });
 });
