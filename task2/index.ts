407e6f65fb1de91c9a1d040be6701ba2a94f12b4
Jakub Wierzchowiec <jwierzchowiec@griddynamics.com>
2026-01-29 17:10:21 +0100

    task 2

--- task2/index.ts
+++ task2/index.ts
@@ -1,13 +1,39 @@
+export interface ValidationResult {
+  isValid: boolean;
+  error: string | null;
+}
+
 interface IQuantityValidator {
-  validate(quantity: number): { isValid: boolean; error: string | null };
+  validate(quantity: number): ValidationResult;
 }
 
 export class QuantityValidator implements IQuantityValidator {
+  private threshold: number;
+  private packageSize: number;
+
   constructor(threshold: number, packageSize: number) {
-    // here some validation logic should be
+    if (threshold < 0) throw new Error('Threshold must be non-negative');
+    if (packageSize <= 0) throw new Error('Package size must be positive');
+    this.threshold = threshold;
+    this.packageSize = packageSize;
   }
 
-  public validate(quantity: number): { isValid: boolean; error: string | null } {
-    throw new Error('Not implemented');
+  validate(quantity: number): ValidationResult {
+    if(quantity <= 0) {
+      return {
+        isValid: false,
+        error: 'Quantity should be positive'
+      }
+    }
+    if(quantity >= this.threshold && quantity % this.packageSize !== 0) {
+      return {
+        isValid: false,
+        error: `Quantity should be divisible by ${this.packageSize}`
+      } 
+    }
+    return {
+      isValid: true,
+      error: null
+    };
   }
 }
--- task2/task2.test.ts
+++ task2/task2.test.ts
@@ -1,7 +1,99 @@
-import { QuantityValidator } from 'tasks/task2';
+import { QuantityValidator, ValidationResult } from 'tasks/task2';
 
 describe('QuantityValidator', () => {
-  it('', () => {
-    // write your first test here :)
+  it('should not return error if quantity is less than threshold', () => {
+    const threshold = 10;
+    const packageSize = 5;
+    const validator = new QuantityValidator(threshold, packageSize);
+    const quantity = 9;
+
+    const actual = validator.validate(quantity);
+
+    expect(actual).toEqual<ValidationResult>({
+      isValid: true,
+      error: null
+    });
+  });
+
+  it('should not return error if quantity is equal to threshold and divisible by package size', () => {
+    const threshold = 10;
+    const packageSize = 5;
+    const validator = new QuantityValidator(threshold, packageSize);
+    const quantity = threshold;
+
+    const actual = validator.validate(quantity);
+
+    expect(actual).toEqual<ValidationResult>({
+      isValid: true,
+      error: null
+    });
+  });
+
+  it('should not return error if quantity is greater than threshold and divisible by package size', () => {
+    const threshold = 10;
+    const packageSize = 5;
+    const validator = new QuantityValidator(threshold, packageSize);
+    const quantity = 15;
+
+    const actual = validator.validate(quantity);
+
+    expect(actual).toEqual<ValidationResult>({
+      isValid: true,
+      error: null
+    });
+  });
+
+  it('should return error if quantity is equal to threshold and indivisible by package size', () => {
+    const threshold = 10;
+    const packageSize = 6;
+    const validator = new QuantityValidator(threshold, packageSize);
+    const quantity = threshold;
+
+    const actual = validator.validate(quantity);
+
+    expect(actual).toEqual<ValidationResult>({
+      isValid: false,
+      error: `Quantity should be divisible by ${packageSize}`
+    });
+  });
+
+  it('should return error if quantity is greater than threshold and indivisible by package size', () => {
+    const threshold = 10;
+    const packageSize = 6;
+    const validator = new QuantityValidator(threshold, packageSize);
+    const quantity = 11;
+
+    const actual = validator.validate(quantity);
+
+    expect(actual).toEqual<ValidationResult>({
+      isValid: false,
+      error: `Quantity should be divisible by ${packageSize}`
+    });
+  });
+
+  it.each([-1, 0])('should return error if quantity is not a positive number', (quantity) => {
+    const threshold = 10;
+    const packageSize = 6;
+    const validator = new QuantityValidator(threshold, packageSize);
+
+    const actual = validator.validate(quantity);
+
+    expect(actual).toEqual<ValidationResult>({
+      isValid: false,
+      error: 'Quantity should be positive'
+    });
+  });
+
+  it('should throw error if threshold is less than 0', () => {
+    const threshold = -1;
+    const packageSize = 6;
+
+    expect(() => new QuantityValidator(threshold, packageSize)).toThrow('Threshold must be non-negative');
+  });
+
+  it.each([-1, 0])('should throw error if package size is not a positive number [%d]', (packageSize) => {
+    const threshold = 10;
+
+    expect(() => new QuantityValidator(threshold, packageSize)).toThrow('Package size must be positive');
   });
 });
